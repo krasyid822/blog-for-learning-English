6fa534480520cdf4deae10ccfae42f15937aa7ab
Rasyid Kurniawan <krasyid822@gmail.com>
2024-04-02 10:59:58 +0700

    update style

---
+++ script.js
@@ -0,0 +1,6 @@
+// Toggle between light and dark theme
+function toggleTheme() {
+    var body = document.body;
+    body.classList.toggle("light-theme");
+    body.classList.toggle("dark-theme");
+  }
